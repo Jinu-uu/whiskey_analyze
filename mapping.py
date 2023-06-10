109bd123a46039bac3768a32bf9081a9c033e4f4
Chaosfire7 <kland2001@naver.com>
2023-06-11 04:24:14 +0900

    define cask_mapping : mapping as whiskey's main info 'cask'

--- mapping.py
+++ mapping.py
@@ -39,3 +39,48 @@ class mapping:
         }
         distillery_mapping = pd.DataFrame(distillery_mapping)
         distillery_mapping.to_csv('./mapping_data/mapping_country.csv', index=False)
+
+    def cask__mapping(self) -> None:
+        '''캐스크 매핑함수'''
+        # 분석할 데이터
+        casktype_mapping = {'Sherry': ['fruit', 'spiciness', 'smoke'], 
+           'Bourbon': ['vanilla', 'sweetness', 'caramel', 'creamy'], 
+           'Burgundy': ['very fruity', 'lightly sweet', 'lightly dry'], 
+           'Madeira': ['spiciness', 'light fruitiness', 'sweetness', 'dryness'], 
+           'Port': ['fruit', 'spiciness'], 
+           'Oloroso': ['deep', 'dark', 'nutty', 'dark ripe fruits'], 
+           'Pedro Ximenez': ['very sweet', 'dark fruits', 'raisins', 'syrup'], 
+           'Fino': ['light fruits', 'sweetness', 'dryness', 'light wood'], 
+           'Manzanilla': ['salty', 'dryness', 'sea flavours', 'fresh', 'some fruit'], 
+           'Amontillado': ['sweetness', 'nutty', 'dry', 'fresh', 'acid'], 
+           'Palo Cortado': ['rich', 'sweet', 'dry', 'sweet spices', 'fruits'], 
+           'Sauternes': ['sweetness', 'zest', 'acidity', 'light fruits'], 
+           'Bordeaux': ['strong red fruits', 'grapes (wine)', 'berries'], 
+           'Tokaji': ['light fresh fruits', 'very sweet'], 
+           'Ruby Port': ['very fruity', 'dark fruits', 'berries'], 
+           'Barolo': ['fruits', 'tannins (bitter)', 'dried fruits', 'heavy aromas'], 
+           'Chardonnay': ['lean', 'crisp', 'acidic', 'tropical fruits'], 
+           'Muscat': ['floral', 'sweet', 'citrus', 'peach'], 
+           'Rum': ['sweet', 'vanilla', 'fruits'], 
+           'Amarone': ['tannins (bitter)', 'dry', 'raisins', 'ripe fruits'],
+           'American oak':['mellow, soft, vanilla, caramel'],
+           'European oak':['spicy', 'bitter', 'stong on the wood'],
+           'Mizunara Oak':['sandal wood', 'coconut', 'oriental spices'],
+            'oak':['vanilla', 'coffee', 'fruit']}
+
+        # 딕셔너리를 pandas DataFrame으로 변환
+        casktype_mapping = pd.DataFrame.from_dict(casktype_mapping, orient='index', columns=['Description1', 'Description2', 'Description3', 'Description4', 'Description5'])
+
+        # cask 컬럼 추가
+        casktype_mapping.insert(loc=0, column='cask', value=casktype_mapping.index)
+
+        # description 컬럼 추가
+        casktype_mapping['description'] = casktype_mapping.values.tolist()
+        casktype_mapping = casktype_mapping[['cask', 'description']]
+
+        # 결과 출력
+        for i, desc in enumerate(casktype_mapping['description']):
+            casktype_mapping['description'][i] = [d for d in desc if d is not None]
+        casktype_mapping = casktype_mapping.reset_index().drop(columns=['index'])
+        casktype_mapping = casktype_mapping.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+        casktype_mapping.to_csv('./mapping_data/mapping_casktype.csv', index=False)
