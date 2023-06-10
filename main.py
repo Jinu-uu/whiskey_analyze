544313921d690adcc2d316cf4a0a75f2ab904091
Chaosfire7 <kland2001@naver.com>
2023-06-11 03:57:40 +0900

    cralwer class instance add at main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import crawler
 
-
-if __name__ == '__main__': pass
+if __name__ == '__main__':
+    crawling_process = crawler.crawler()
+    crawling_process.whisky_base_crawler()
+    crawling_process.raw_data_preprocessing()
+        
