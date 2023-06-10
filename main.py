dd06cf346b6da25c60d5f62b5dc9bd14104b38c6
Chaosfire7 <kland2001@naver.com>
2023-06-11 04:27:03 +0900

    solve pandas warning masseage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import crawler
 import mapping
+import pandas as pd
 import os
 
 def crawling_skip(name) -> bool:
@@ -11,6 +12,7 @@ def crawling_skip(name) -> bool:
     return False
 
 if __name__ == '__main__':
+    pd.set_option('mode.chained_assignment',  None)
     if crawling_skip('whiskeybase.csv')  == False and crawling_skip('whiskeybase_raw.csv')  == False: 
         crawling_process = crawler.crawler()
         crawling_process.whisky_base_crawler()
