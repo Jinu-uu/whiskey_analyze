1e80ffb513d227cc875b15468cb1d8a67a2791ad
LilStar <82719310+Jinu-uu@users.noreply.github.com>
2023-06-11 04:28:37 +0900

    Whiskey mapping branch is done (#2)
    * mapping class initialize and define distillery_mapping : mapping as distilery
    
    * define cask_mapping : mapping as whiskey's main info 'cask'
    
    * add mapping data from base mapping data
    
    * define main method : mapping class' all method run
    
    * add mapping class instance at main.py
    
    * solve pandas warning masseage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import crawler
+import mapping
+import pandas as pd
 import os
 
 def crawling_skip(name) -> bool:
@@ -10,8 +12,11 @@ def crawling_skip(name) -> bool:
     return False
 
 if __name__ == '__main__':
+    pd.set_option('mode.chained_assignment',  None)
     if crawling_skip('whiskeybase.csv')  == False and crawling_skip('whiskeybase_raw.csv')  == False: 
         crawling_process = crawler.crawler()
         crawling_process.whisky_base_crawler()
         crawling_process.raw_data_preprocessing()
         
+    mapping_process = mapping.mapping()
+    mapping_process.main()
