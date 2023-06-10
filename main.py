3c19a264402c5ca966a831092028cd61e5e2244c
Chaosfire7 <kland2001@naver.com>
2023-06-11 03:58:42 +0900

    if already have crawling data, skip crawling method

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import crawler
+import os
+
+def crawling_skip(name) -> bool:
+    for (path, dir, files) in os.walk("./data"):
+        for filename in files:
+            ext = os.path.splitext(filename)[-1]
+            if filename == name:
+                return True
+    return False
 
 if __name__ == '__main__':
-    crawling_process = crawler.crawler()
-    crawling_process.whisky_base_crawler()
-    crawling_process.raw_data_preprocessing()
+    if crawling_skip('whiskeybase.csv')  == False and crawling_skip('whiskeybase_raw.csv')  == False: 
+        crawling_process = crawler.crawler()
+        crawling_process.whisky_base_crawler()
+        crawling_process.raw_data_preprocessing()
         
