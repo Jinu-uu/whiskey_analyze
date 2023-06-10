b2970cbfccb87645caf28358787c653a78da79c0
LilStar <82719310+Jinu-uu@users.noreply.github.com>
2023-06-11 04:00:03 +0900

    Whiskey crawler branch is done (#1)
    * crawler class initialize and define whisky_base_cralwer method for crawling at 'whisky base'
    
    * define folder_detect method : if 'folder_name'(variable) directory not found, make 'folder_name' directory
    
    * define raw_data_preprocessing method : preprocessing raw data from whisky_base_crawler method
    
    * cralwer class instance add at main.py
    
    * if already have crawling data, skip crawling method

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import crawler
+import os
 
+def crawling_skip(name) -> bool:
+    for (path, dir, files) in os.walk("./data"):
+        for filename in files:
+            ext = os.path.splitext(filename)[-1]
+            if filename == name:
+                return True
+    return False
 
-if __name__ == '__main__': pass
+if __name__ == '__main__':
+    if crawling_skip('whiskeybase.csv')  == False and crawling_skip('whiskeybase_raw.csv')  == False: 
+        crawling_process = crawler.crawler()
+        crawling_process.whisky_base_crawler()
+        crawling_process.raw_data_preprocessing()
+        
