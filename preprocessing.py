ef2ac388db92ebab2e70122b7c5849d16cb5ded1
Chaosfire7 <kland2001@naver.com>
2023-06-11 04:19:18 +0900

    define 3 method for preprocessing(jsontodf, get_cask,replace_german_spanish_chars)

--- preprocessing.py
+++ preprocessing.py
@@ -7,3 +7,36 @@ class preprocessing:
         '''필요한 csv 파일 불러오기'''
         self.casktype_df = pd.read_csv('./mapping_data/mapping_casktype.csv', encoding='latin1').set_index('cask')
         self.whiskey_df = pd.read_csv('./data/whiskeybase.csv')
+
+    def jsontodf(self, df:pd.DataFrame, target_list:list)->pd.DataFrame:
+        '''ast 라이브러리를 사용해서 csv에 json형식으로 되어있는 컬럼을 바꿔주는 함수이다.'''
+        for idx in range(len(df)):
+            for column in target_list:
+                tmp = ast.literal_eval(df[column][idx])
+                df[column][idx] = tmp
+        return df
+
+    
+    def get_cask(self, casks:list) -> list:
+        '''casktype_df에서 casks를 찾아 반환해주는 함수이다.'''
+        cask_list = []
+        for cask in casks:
+            try:
+                cask_list += self.casktype_df.at[cask, 'description']
+            except:
+                pass
+        return ' '.join(cask_list * 2)
+
+    def replace_german_spanish_chars(self,text):
+        '''크롤링한 사이트 특성상 독일어와 스페인어가 많기 때문에, 문자를 바꿔준다.'''
+        # 독일어와 스페인어 알파벳 대응표 with chatgpt
+        german_chars = {'Ä': 'Ae', 'Ö': 'Oe', 'Ü': 'Ue', 'ä': 'ae', 'ö': 'oe', 'ü': 'ue', 'ß': 'ss'}
+        spanish_chars = {'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U', 'Ü': 'U', 'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 'ü': 'u', 'ñ': 'n'}
+
+        # 문자열에서 독일어와 스페인어 알파벳을 영어 알파벳으로 대체
+        for char, repl in german_chars.items():
+            text = text.replace(char, repl)
+        for char, repl in spanish_chars.items():
+            text = text.replace(char, repl)
+
+        return text
