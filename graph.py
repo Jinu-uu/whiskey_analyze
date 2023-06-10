11698bef59298ac5c5773407cb39c1fd36d6ae42
Chaosfire7 <kland2001@naver.com>
2023-06-11 04:50:04 +0900

    add define method plot_number_line and create_text : whiskey;s aroma and weight visualize

--- graph.py
+++ graph.py
@@ -46,3 +46,52 @@ class graph:
 
         #타이틀 달아줌
         plt.title(self.whiskey_variety_vectors_normalized['name'][number].split(',')[0], size=13, color='black', y=1.2)
+
+    def plot_number_line(self, gs, n, dot_color, data, number) -> None:
+        '''바디감을 시각화하는 함수'''
+        #그래프 및 x축, y축 멈위 설정
+        ax = plt.subplot(gs[n])
+        ax.set_xlim(-1, 2)
+        ax.set_ylim(0, 3)
+
+        #선 draw 및 범위, 높이 설정
+        xmin = 0
+        xmax = 1
+        y = 1
+        height = 0.2
+
+        #선 시각화
+        plt.hlines(y, xmin, xmax)
+        plt.vlines(xmin, y - height / 2., y + height / 2., colors='black')
+        plt.vlines(xmax, y - height / 2., y + height / 2., colors='black')
+
+        #점 시각화
+        px = self.whiskey_variety_vectors_normalized['weight'][number]
+        plt.plot(px, y, 'ko', ms = 10, mfc = dot_color)
+
+        #이제 시각화한 그래프 아래에 바디감 출력
+        plt.text(xmin - 0.1, y, 'Light-Bodied', horizontalalignment='right', fontsize=11, color='grey')
+        plt.text(xmax + 0.1, y, 'Full-Bodied', horizontalalignment='left', fontsize=11, color='grey')
+
+        plt.axis('off')
+
+    def create_text(self, gs, n, data, number) -> None:
+        '''위스키 향에 대한 정보를 그래프 아래에 출력해주는 함수'''
+        data = list(data)
+        ax = plt.subplot(gs[n])
+        
+        ax.set_xticks([])
+        ax.set_yticks([])
+        for spine in ax.spines.values():
+            spine.set_visible(False)
+        ax.invert_yaxis()
+        
+        #텍스트 만든 뒤
+        text = 'Wshikey Aromas:\n'
+        for x in range(len(self.whiskey_variety_vectors_normalized['relative_frequency'][number])):
+            if x == 5: break
+            text += self.whiskey_variety_vectors_normalized['relative_frequency'][number][x][0]
+            text += '\n'
+        text = text[:-1]
+        #출력
+        ax.text(x=0, y=1, s=text, fontsize=12, color='grey')
